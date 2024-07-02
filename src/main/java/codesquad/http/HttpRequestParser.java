5b87baf221a3b83172877c185d623740476f29a8
김수현 <i960107@naver.com>
2024-07-03 07:31:27 +0900

    feat: inputstream을 읽어서 Request객체로 파싱하는 기능 추가

---
+++ src/main/java/codesquad/http/HttpRequestParser.java
@@ -0,0 +1,96 @@
+package codesquad.http;
+
+import static codesquad.IOUtil.readLine;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequestParser {
+
+    private static final String CONTENT_LENGTH_HEADER = "Content-Length";
+
+    private static final String HOST_HEADER = "Host";
+
+    public static Request parse(InputStream inputStream) throws IOException {
+        BufferedInputStream input = new BufferedInputStream(inputStream);
+
+        String requestLine = parseRequestLine(input);
+        String[] requestLineParts = splitRequestLine(requestLine);
+        String method = requestLineParts[0];
+        String path = requestLineParts[1];
+        String protocol = requestLineParts[2];
+
+        Map<String, String> headers = parseHeaders(input);
+        if (!headers.containsKey(HOST_HEADER)) {
+            throw new IOException("host header not exist");
+        }
+
+        String host = headers.get(HOST_HEADER);
+
+        Map<String, String> parameters = parseParameters(input);
+
+        byte[] body = null;
+        if (headers.containsKey(CONTENT_LENGTH_HEADER)) {
+            int contentLength = Integer.parseInt(headers.get(CONTENT_LENGTH_HEADER));
+            body = parseBody(input, contentLength);
+        }
+
+        return new WasRequest(
+                method,
+                path,
+                protocol,
+                host,
+                headers,
+                parameters,
+                body
+        );
+    }
+
+    private static String parseRequestLine(BufferedInputStream input) throws IOException {
+        String requestLine = readLine(input);
+        if (requestLine == null || requestLine.isEmpty()) {
+            throw new IOException("empty request line");
+        }
+        return requestLine;
+    }
+
+    private static String[] splitRequestLine(String requestLine) throws IOException {
+        String[] parts = requestLine.split(" ");
+        if (parts.length != 3) {
+            throw new IOException("invalid request line");
+        }
+        return parts;
+    }
+
+    private static Map<String, String> parseHeaders(BufferedInputStream input) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        String headerLine;
+        while (!(headerLine = readLine(input)).isEmpty()) {
+            String[] headerParts = headerLine.split(": ");
+            if (headerParts.length != 2) {
+                throw new IOException("invalid header line");
+            }
+            headers.put(headerParts[0], headerParts[1]);
+        }
+        return headers;
+    }
+
+    private static Map<String, String> parseParameters(BufferedInputStream input) throws IOException {
+        //todo parameter 있는 경우 parameter로 파싱 필요
+        Map<String, String> parameters = new HashMap<>();
+        return parameters;
+    }
+
+    private static byte[] parseBody(BufferedInputStream input, int contentLength) throws IOException {
+        byte[] body = new byte[contentLength];
+        int read = input.read(body, 0, contentLength);
+        if (read != contentLength) {
+            throw new IOException("incomplete body read");
+        }
+        return body;
+    }
+
+}
