c9ce219b71a638337c9abf8cbbefa23a94ba8898
김수현 <i960107@naver.com>
2024-07-10 07:28:07 +0900

    feat: IOUtil readToSize() 추가
    - 기존 StringBuffer -> StringBuilder() 사용하도록 수정. StringBuffer 지역변수로 사용하기 때문에 멀티스레드 고려할 필요 없음
    - 지정된 크기만큼 읽는 메소드 추가

--- src/main/java/codesquad/util/IOUtil.java
+++ src/main/java/codesquad/util/IOUtil.java
@@ -2,6 +2,7 @@ package codesquad.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
@@ -10,7 +11,9 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.util.Locale;
 
-public class IOUtil {
+public final class IOUtil {
+
+    public static final String UTF8 = "UTF-8";
 
     private static final char CR = '\r';
 
@@ -18,7 +21,7 @@ public class IOUtil {
 
     public static String readLine(InputStream input) throws IOException {
         int ch;
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         while ((ch = input.read()) != -1) {
             if (ch == CR) {
                 continue;
@@ -30,6 +33,31 @@ public class IOUtil {
         }
 
         return ch == -1 && sb.isEmpty() ? null : sb.toString();
+    }
+
+    public static String readLine(InputStreamReader input) throws IOException {
+        int ch;
+        StringBuilder sb = new StringBuilder();
+        while ((ch = input.read()) != -1) {
+            if (ch == CR) {
+                continue;
+            } else if (ch == LF) {
+                break;
+            } else {
+                sb.append((char) ch);
+            }
+        }
+
+        return ch == -1 && sb.isEmpty() ? null : sb.toString();
+    }
+
+    public static byte[] readToSize(InputStream input, int size) throws IOException {
+        byte[] buffer = new byte[size];
+        int read = input.read(buffer, 0, size);
+        if (read != size) {
+            throw new IOException("Could not read " + size + " bytes");
+        }
+        return buffer;
     }
 
     public static void writeLine(OutputStream output, String str) throws IOException {
@@ -47,4 +75,5 @@ public class IOUtil {
                 .toFormatter(Locale.US);
         return now.format(formatter) + "GMT";
     }
+
 }
