898f868a9c47e116fbf78c24170bff02a1b05128
김수현 <i960107@naver.com>
2024-07-10 09:51:57 +0900

    test: HTTP 요청 파라미터 파싱 테스트 추가

--- src/test/java/codesquad/http/HttpRequestParserTest.java
+++ src/test/java/codesquad/http/HttpRequestParserTest.java
@@ -1,5 +1,6 @@
 package codesquad.http;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,6 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import codesquad.http.exception.HttpProtocolException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,34 +18,87 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class HttpRequestParserTest {
 
-    @DisplayName("http 요청을 파싱한다.")
+    @DisplayName("http 요청을 파싱한다.- GET query string 포함")
     @Test
-    void testParseRequest() throws IOException {
+    void testParseRequestGETQueryString() throws IOException {
+        //given
         InputStream input = getClass().getClassLoader().getResourceAsStream("user-create-request.txt");
+
+        //when
         WasRequest request = new WasRequest();
         HttpRequestParser.parse(request, input);
 
-        assertEquals("HTTP/1.1", request.getProtocol());
-        assertEquals("localhost:8080", request.getHost());
-        assertEquals(HttpMethod.GET, request.getMethod());
-        assertEquals("/user/create", request.getPath());
-        assertEquals(4, request.getQueryString().size());
-        assertTrue(request.getQueryString().containsKey("password"));
-        assertTrue(request.getQueryString().containsKey("name"));
-        assertTrue(request.getQueryString().containsKey("email"));
-        assertTrue(request.getQueryString().containsKey("userId"));
-        assertEquals("password", request.getQueryString().get("password"));
-        assertEquals("javajigi", request.getQueryString().get("userId"));
-        assertEquals("박재성", request.getQueryString().get("name"));
-        assertEquals("javajigi@slipp.net", request.getQueryString().get("email"));
-        assertEquals(3, request.getHeaders().getHeaders().size());
-        assertEquals("keep-alive", request.getHeader(HttpHeaders.CONNECTION_HEADER).get().getOnlyValue().get());
-        assertEquals("*/*", request.getHeader(HttpHeaders.ACCEPT).get().getOnlyValue().get());
+        //then
+        assertRequestLine(request, "HTTP/1.1", "/user/create", HttpMethod.GET);
+
+        assertEquals(3, request.getHeaders().size());
+        assertHeader(request, HttpHeaders.CONNECTION_HEADER, "keep-alive");
+        assertHeader(request, HttpHeaders.HOST_HEADER, "localhost:8080");
+        assertHeader(request, HttpHeaders.ACCEPT, "*/*");
+
+        Map<String, List<String>> expectedParameters = new HashMap<>();
+        expectedParameters.put("password", List.of("password"));
+        expectedParameters.put("name", List.of("박재성"));
+        expectedParameters.put("email", List.of("javajigi@slipp.net"));
+        expectedParameters.put("userId", List.of("javajigi"));
+        assertRequestParameter(request, expectedParameters);
+    }
+
+    private static void assertRequestLine(WasRequest request,
+                                          String expectedProtocol,
+                                          String expectedPath,
+                                          HttpMethod expectedMethod) {
+        assertEquals(expectedProtocol, request.getProtocol());
+        assertEquals(expectedPath, request.getPath());
+        assertEquals(expectedMethod, request.getMethod());
+    }
+
+    private static void assertHeader(WasRequest request,
+                                     String expectedHeaderKey,
+                                     String expectedHeaderValue) {
+        assertTrue(request.getHeader(expectedHeaderKey).isPresent());
+        assertEquals(expectedHeaderValue, request.getHeader(expectedHeaderKey).get().getSingleValue().get());
+    }
+
+    private static void assertRequestParameter(WasRequest request,
+                                               Map<String, List<String>> expectedParameters) {
+        assertEquals(expectedParameters.size(), request.getParameterMap().size());
+        assertThat(request.getParameterMap())
+                .hasSize(expectedParameters.size())
+                .isEqualTo(expectedParameters);
+    }
+
+    @DisplayName("http 요청을 파싱한다. - POST formdata")
+    @Test
+    void testParseRequestPOSTFormData() throws IOException {
+        //given
+        InputStream input = getClass().getClassLoader().getResourceAsStream("user-create-form-data.txt");
+
+        //when
+        WasRequest request = new WasRequest();
+        HttpRequestParser.parse(request, input);
+
+        //then
+        assertRequestLine(request, "HTTP/1.1", "/create", HttpMethod.POST);
+
+        assertEquals(3, request.getHeaders().size());
+        assertHeader(request, HttpHeaders.CONTENT_TYPE_HEADER, "application/x-www-form-urlencoded");
+        assertHeader(request, HttpHeaders.HOST_HEADER, "localhost:8080");
+        assertHeader(request, HttpHeaders.CONTENT_LENGTH_HEADER, "63");
+
+        Map<String, List<String>> expectedParameters = new HashMap<>();
+        expectedParameters.put("username", List.of("iwer"));
+        expectedParameters.put("nickname", List.of("박재성"));
+        expectedParameters.put("password", List.of("123"));
+        assertRequestParameter(request, expectedParameters);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"invalid-request-1.txt", "invalid-request-2.txt", "invalid-request-3.txt"})
-    @DisplayName("http 프로토콜을 지키지 않은 경우 예외를 던진다.")
+    @ValueSource(strings = {
+            "invalid-request-1-nohost.txt",
+            "invalid-request-2-noblankline.txt",
+            "invalid-request-3-nohttpmethod.txt"})
+    @DisplayName("http 프로토콜을 지키지 않은 경우 예외를 던진다. : {0}")
     void testThrowsExceptionWhenParseInvalidRequest(String fileName) throws IOException {
         InputStream input = getClass().getClassLoader().getResourceAsStream(fileName);
         WasRequest request = new WasRequest();
