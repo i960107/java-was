f93da4a60878b1768cdd0ec6fa89666bf1624eb6
김수현 <i960107@naver.com>
2024-07-10 09:52:25 +0900

    test: Handler 테스트 수정

--- src/test/java/codesquad/server/HandlerContextTest.java
+++ src/test/java/codesquad/server/HandlerContextTest.java
@@ -29,14 +29,18 @@ class HandlerContextTest {
         this.handlerContext = HandlerContext.getInstance();
     }
 
-    private WasRequest getWasRequest(String path) {
-        return new WasRequest(HttpMethod.GET, path, "HTTP1.1", "localhost", HttpHeaders.getDefault(), null, null, null);
+    private WasRequest get(String path) {
+        return new WasRequest(HttpMethod.GET, path, "HTTP1.1", "localhost", HttpHeaders.getDefault(), null, null);
+    }
+
+    private WasRequest post(String path) {
+        return new WasRequest(HttpMethod.POST, path, "HTTP1.1", "localhost", HttpHeaders.getDefault(), null, null);
     }
 
     @DisplayName("정적 리소스에 대한 요청시 DefaultHandler가 처리한다.")
     @Test
     void testStaticResourceRequest() {
-        WasRequest wasRequest = getWasRequest("/index.html");
+        WasRequest wasRequest = get("/index.html");
 
         Handler handler = handlerContext.getMappedHandler(wasRequest);
         assertEquals(handler.getClass(), DefaultHandler.class);
@@ -45,16 +49,19 @@ class HandlerContextTest {
     @DisplayName("어플리케이션 요청시 UrlMappingHandler가 처리한다.")
     @Test
     void testApplicationRequest() {
-        WasRequest wasRequest = getWasRequest("/create");
+
+        WasRequest wasRequest = post("/create");
 
         Handler handler = handlerContext.getMappedHandler(wasRequest);
+
         assertEquals(handler.getClass(), UrlMappingHandler.class);
     }
 
     @DisplayName("등록되지 않은 경로로 요청시 예외가 발생한다.")
     @Test
     void testApplicationRequestToUnregisteredPath() {
-        WasRequest wasRequest = getWasRequest("/create-user");
+
+        WasRequest wasRequest = get("/create-user");
 
         assertThrows(NoMatchHandlerException.class, () -> handlerContext.getMappedHandler(wasRequest));
     }
