b43a45e404d0cf41833fe227a317a5ecf2aa89ee
김수현 <i960107@naver.com>
2024-07-17 11:35:19 +0900

    fix: ServerContext 초기화 로직 수정
    - 기존 생성자에서 초기화
    - 수정 후 setter로 초기화. 의존성이 있는 필드에 대해서 필드마다 초기화 순서 조절 가능.
    - 하나의 connection pool을 생성후 JdbcTemplate객체를 dao 객체들이 공유하도록 수정

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,17 +1,18 @@
 package codesquad;
 
 import codesquad.application.db.DBConfig;
-import codesquad.application.db.InMemoryUserRepository;
+import codesquad.application.db.JdbcTemplate;
+import codesquad.application.db.PostDao;
+import codesquad.application.db.UserDao;
 import codesquad.application.handler.IndexHandler;
+import codesquad.application.handler.PostWriteHandler;
 import codesquad.application.handler.UserListHandler;
 import codesquad.application.handler.UserLoginHandler;
 import codesquad.application.handler.UserLogoutHandler;
 import codesquad.application.handler.UserRegisterHandler;
-import codesquad.was.dbcp.ConnectionPool;
-import codesquad.was.dbcp.DefaultConnectionPool;
 import codesquad.was.http.HttpProtocol;
-import codesquad.was.server.ServerContext;
 import codesquad.was.server.DefaultHandler;
+import codesquad.was.server.ServerContext;
 import codesquad.was.server.authenticator.DefaultAuthenticator;
 import codesquad.was.server.session.InMemorySessionManager;
 import java.io.IOException;
@@ -20,27 +21,23 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        InMemoryUserRepository userRepository = new InMemoryUserRepository();
         DBConfig dbConfig = DBConfig.getDBConfig("application.properties");
-        ConnectionPool connectionPool = new DefaultConnectionPool(
-                dbConfig.getUrl(),
-                dbConfig.getUsername(),
-                dbConfig.getPassword(),
-                dbConfig.getMaxPoolSize(),
-                dbConfig.getMinIdle(),
-                dbConfig.getConnectionTimeout(),
-                dbConfig.getIdleTimeout()
-        );
-        ServerContext context = new ServerContext(
-                new InMemorySessionManager(),
-                new DefaultAuthenticator(userRepository),
-                connectionPool
-        );
-        context.addHandler("/create", new UserRegisterHandler(userRepository));
+
+        ServerContext context = new ServerContext();
+        context.setSessionManager(new InMemorySessionManager());
+        context.setConnectionPool(dbConfig);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(context.getConnectionPool());
+        UserDao userDao = new UserDao(jdbcTemplate);
+        context.setAuthenticator(new DefaultAuthenticator(userDao));
+
+        context.addHandler("/create", new UserRegisterHandler(userDao));
         context.addHandler("/login", new UserLoginHandler());
         context.addHandler("/logout", new UserLogoutHandler());
-        context.addHandler("/user/list", new UserListHandler(userRepository));
+        context.addHandler("/user/list", new UserListHandler(userDao));
         context.addHandler("/index.html", new IndexHandler());
+        context.addHandler("/index", new IndexHandler());
+        PostDao postDao = new PostDao(jdbcTemplate);
+        context.addHandler("/post", new PostWriteHandler(postDao));
         context.addHandler("/", new DefaultHandler());
 
         HttpProtocol httpProtocol = new HttpProtocol(context);
