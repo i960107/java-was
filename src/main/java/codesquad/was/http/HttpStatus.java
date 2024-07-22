8904e8ff8b11ff61a67d00e73b9cbba58a98cec8
김수현 <i960107@naver.com>
2024-07-23 01:03:48 +0900

    feat: HttpStatus Message 필드 추가
    - 응답 메시지의 status message 대소문자 컨벤션 따르기 위해 status 필드 추가

--- src/main/java/codesquad/was/http/HttpStatus.java
+++ src/main/java/codesquad/was/http/HttpStatus.java
@@ -1,26 +1,33 @@
 package codesquad.was.http;
 
 public enum HttpStatus {
-    OK("200"),
-    CREATED("201"),
-    MOVED_PERMANENTLY("301"),
-    FOUND("302"),
-    NOT_MODIFIED("304"),
-    BAD_REQUEST("400"),
-    UNAUTHORIZED("401"),
-    FORBIDDEN("403"),
-    NOT_FOUND("404"),
-    METHOD_NOT_ALLOWED("405"),
-    INTERNAL_SERVER_ERROR("500"),
-    BAD_GATEWAY("502");
+    OK("200", "OK"),
+    CREATED("201", "Created"),
+    MOVED_PERMANENTLY("301", "Moved Permanently"),
+    FOUND("302", "Found"),
+    NOT_MODIFIED("304", "Not Modified"),
+    BAD_REQUEST("400", "Bad Request"),
+    UNAUTHORIZED("401", "Unauthorized"),
+    FORBIDDEN("403", "Forbidden"),
+    NOT_FOUND("404", "Not Found"),
+    METHOD_NOT_ALLOWED("405", "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR("500", "Internal Server Error"),
+    BAD_GATEWAY("502", "Bad Gateway");
 
     private final String code;
 
-    HttpStatus(String code) {
+    private final String message;
+
+    HttpStatus(String code, String message) {
         this.code = code;
+        this.message = message;
     }
 
     public String getCode() {
         return code;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
